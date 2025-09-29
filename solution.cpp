8674c77f1faf71bc593924963bd63f48866327a4
jlm22010 <jonahmerriam2@gmail.com>
2025-09-29 10:33:15 -0400

    made copy of completed code with correct name

---
+++ solution.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <cstring>   // for strlen, strcpy
+
+// TODO: function prototypes
+void addStudent(char *name, double gpa, char *names[], double gpas[], int &size, int capacity);
+void updateGPA(double *gpaPtr, double newGpa);
+void printStudent(const char *name, const double &gpa);
+double averageGPA(const double gpas[], int size);
+
+// TODO: implement addStudent
+void addStudent(char *name, double gpa, char *names[], double gpas[], int &size, int capacity){
+	if(size < capacity){
+		names[size] = new char[256];
+		
+		strcpy(names[size], name);
+		gpas[size] = gpa;
+
+		size++;
+
+		return;
+	}
+	
+	// shouldn't get here
+	throw "List full";
+}
+
+// TODO: implement updateGPA
+void updateGPA(double *gpaPtr, double newGpa){
+	*gpaPtr = newGpa;
+}
+
+// TODO: implement printStudent
+void printStudent(const char *name, const double &gpa){
+	std::cout << name << " | " << gpa << std::endl;
+}
+
+// TODO: implement averageGPA
+double averageGPA(const double gpas[], int size){
+	if(size != 0){
+		double sum = 0;
+
+		for(int i = 0; i < size; i++){
+			sum += gpas[i];
+		}
+
+		return sum / size;
+	}
+	
+	// shoudln't get here
+	throw "No students";
+	return -1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./program <capacity>" << std::endl;
+        return 1;
+    }
+
+    int capacity = std::stoi(argv[1]);
+    char* names[capacity];
+    double gpas[capacity];
+    int size = 0;
+
+    int choice;
+    do {
+        std::cout << "Menu:\n";
+        std::cout << "1. Add student\n";
+        std::cout << "2. Update GPA\n";
+        std::cout << "3. Print all students\n";
+        std::cout << "4. Compute average GPA\n";
+        std::cout << "5. Quit\n";
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                // TODO: implement menu logic
+		// get name
+		std::cout << "Please enter a name: ";
+		std::cin.get();
+		char *name = new char[256];
+		std::cin.getline(name, 256);
+		std::cout << std::endl;
+
+		// get gpa
+		std::cout << "Please enter a GPA: ";
+		double gpa = 0;
+		std::cin >> gpa;
+		std::cout << std::endl;
+
+		// call function
+		try{
+			addStudent(name, gpa, names, gpas, size, capacity);
+		}
+		catch(const char *msg){
+			std::cout << msg << std::endl;
+		}
+
+		// cleanup
+                delete[] name;
+		break;
+            }
+            case 2: {
+                // TODO: implement menu logic
+		// get index of gpa
+		std::cout << "Please enter an index: ";
+		int idx = 0;
+		std::cin >> idx;
+		std::cout << std::endl;
+
+		// get new gpa
+		std::cout << "Please enter a new GPA: ";
+		double newGpa = 0;
+		std::cin >> newGpa;
+		std::cout << std::endl;
+
+		// call function
+		updateGPA(&gpas[idx], newGpa);
+
+		// cleanup
+                break;
+            }
+            case 3: {
+                // TODO: implement menu logic
+		// make sure the list isn't empty
+		if(size == 0){
+			std::cout << "No students" << std::endl;
+			break;
+		}
+		
+                // iterate over the list
+		for(int i = 0; i < size; i++){
+			printStudent(names[i], gpas[i]);
+		}
+
+		// cleanup
+		break;
+            }
+            case 4: {
+                // TODO: implement menu logic
+		// call the function
+		try{
+			double avg = averageGPA(gpas, size);
+
+			std::cout << "Average GPA: " << static_cast<int>(avg) << std::endl;
+		}
+		catch(const char *msg){
+			std::cout << msg << std::endl;
+		}
+		// cleanup
+		break;
+            }
+            case 5: {
+                std::cout << "Goodbye!" << std::endl;
+                break;
+            }
+            default: {
+                std::cout << "Invalid choice" << std::endl;
+            }
+        }
+    } while (choice != 5);
+
+    // TODO: free memory
+	for(int i = 0; i < size; i++){
+		delete[] names[i];
+	}
+
+    return 0;
+}
